cc60c6c77b44a1f89e602c1b4258d9e1c8cdc41d
Rene Hexel <r.hexel@griffith.edu.au>
2025-06-28 08:48:42 +1000

    Use Swift 6.1 and swift-testing.

--- Tests/ScriptingBridgeTests/ScriptingBridgeTests.swift
+++ Tests/ScriptingBridgeTests/ScriptingBridgeTests.swift
@@ -1,12 +1,6 @@
-import XCTest
+import Testing
 @testable import ScriptingBridge
 
-final class ScriptingBridgeTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+@Test func example() throws {
+    #expect(Bool(true) == true)
 }
